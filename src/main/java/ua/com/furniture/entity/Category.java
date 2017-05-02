58b5d034f1e669953ad781bc6c387b43d70bbf75
Roman-Pidkost <romko0511@gmail.com>
2017-05-02 10:46:42 +0300

    fixing looping in json

--- src/main/java/ua/com/furniture/entity/Category.java
+++ src/main/java/ua/com/furniture/entity/Category.java
@@ -1,12 +1,12 @@
 package ua.com.furniture.entity;
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
+//@Data
 @Entity
 public class Category {
 
@@ -16,6 +16,34 @@ public class Category {
 
     private String name;
 
+    @JsonProperty
     @OneToMany(mappedBy = "category")
     private List<Subcategory> subcategories = new ArrayList<>();
+
+    public Category() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Subcategory> getSubcategories() {
+        return subcategories;
+    }
+
+    public void setSubcategories(List<Subcategory> subcategories) {
+        this.subcategories = subcategories;
+    }
 }
--- src/main/java/ua/com/furniture/entity/Subcategory.java
+++ src/main/java/ua/com/furniture/entity/Subcategory.java
@@ -1,13 +1,17 @@
 package ua.com.furniture.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Subcategory {
 
@@ -17,10 +21,16 @@ public class Subcategory {
 
     private String name;
 
+    @JsonIgnore
     @ManyToOne
     private Category category;
 
     @JsonIgnore
     @OneToMany(mappedBy = "subcategory")
     private List<Commodity> commodities = new ArrayList<>();
+
+    public Subcategory(String name, Category category) {
+        this.name = name;
+        this.category = category;
+    }
 }
